613f600bed077c38175e52f6f671af8f7a77ed1c
Kalaivani0706 <kvani0706@gmail.com>
2025-07-30 15:42:27 +0530

    Update Signin.jsx

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,15 +1,1 @@
-import React from 'react'
 
-function Signin() {
-  return (
-    <>
-    <h1>sign in here!!</h1>
-    {/* <form>
-      <label>Userid</label>
-      <input type="text" id="user" class="userid" value={} placeholder='enter your user id here!1'/>
-    </form> */}
-    </>
-  )
-}
-
-export default Signin;
